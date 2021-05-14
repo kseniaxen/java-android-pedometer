5b3c74115c2f56a37b8c8782b360e7e7cccc0dfa
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-05-14 12:39:40 +0300

    Show activity for week data and TODO activity for month

--- app/src/main/java/org/ksens/java/android/pedometer/StatisticsActivity.java
+++ app/src/main/java/org/ksens/java/android/pedometer/StatisticsActivity.java
@@ -1,10 +1,18 @@
 package org.ksens.java.android.pedometer;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
@@ -18,60 +26,118 @@ import com.anychart.enums.Position;
 import com.anychart.enums.TooltipPositionMode;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StatisticsActivity extends AppCompatActivity {
 
+    private IRecordDao recordDao = Global.recordDao;
+    private DecimalFormat df;
+    private int KEY_WEEK=7;
+    private int KEY_MONTH=12;
+    private Button statisticsWeekButton;
+    private Button statisticsMonthButton;
+    private AnyChartView statisticsChartWeekAnyChartView;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
+        TextView statisticsTotalKmTextView = findViewById(R.id.statisticsTotalKmTextView);
+        TextView statisticsTotalStepsTextView = findViewById(R.id.statisticsTotalStepsTextView);
+        statisticsWeekButton = findViewById(R.id.statisticsWeekButton);
+        statisticsMonthButton = findViewById(R.id.statisticsMonthButton);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        statisticsChartWeekAnyChartView = findViewById(R.id.statisticsChartWeekAnyChartView);
+        TableLayout statisticsTableLayout = findViewById(R.id.statisticsTableLayout);
+
+        Double totalKm = recordDao.findAll().stream().mapToDouble(x -> x.getKm()).sum();
+        Integer totalSteps = recordDao.findAll().stream().mapToInt(x -> x.getSteps()).sum();
+        df = new DecimalFormat("#.##");
+        statisticsTotalKmTextView.setText(df.format(totalKm));
+        statisticsTotalStepsTextView.setText(totalSteps.toString());
+
+        onButtonWeekState();
+
+        statisticsWeekButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonWeekState();
+            }
+        });
+
+        statisticsMonthButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /*
+                statisticsChartWeekAnyChartView.setVisibility(View.INVISIBLE);
+                statisticsWeekButton.setBackgroundColor(getResources().getColor(R.color.background));
+                statisticsWeekButton.setTextColor(getResources().getColor(R.color.textToday));
+                statisticsMonthButton.setBackgroundColor(getResources().getColor(R.color.textGoal));
+                statisticsMonthButton.setTextColor(getResources().getColor(R.color.black));
+
+                List<RecordItem> recordItemsData = RecordItem.findWithQuery(RecordItem.class, "SELECT DISTINCT date FROM RECORD_ITEM");
+                List<String> date = recordItemsData.stream().map(RecordItem::getDate).collect(Collectors.toList());
+
+                List <String> months = new ArrayList<>();
+                for (String d:date) {
+                    Date dateFormat = null;
+                    try {
+                        dateFormat = new SimpleDateFormat("dd.MM.yyyy").parse(d);
+                        DateFormat dateFormatMonth = new SimpleDateFormat("MM");
+                        months.add(dateFormatMonth.format(dateFormat));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+                List<Integer> recordItemSteps = new ArrayList<>();
+                List<String> uniqueMonths = months.stream().distinct().collect(Collectors.toList());
+                int steps=0;
+                for (String month:uniqueMonths){
+                    for (RecordItem recordItem:recordDao.findAll()) {
+                        try {
+                            Date dateFormat = new SimpleDateFormat("dd.MM.yyyy").parse(recordItem.getDate());
+                            DateFormat dateFormatMonth = new SimpleDateFormat("MM");
+                            if(dateFormatMonth.format(dateFormat).equals(month)){
+                                steps += recordItem.getSteps();
+                            }
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    recordItemSteps.add(steps);
+                    steps = 0;
+                }
+
+                Collections.reverse(recordItemSteps);
+                Collections.reverse(uniqueMonths);
+
+                for (int i = 0; i < uniqueMonths.size(); i++){
+                    if(i >= KEY_MONTH){
+                        break;
+                    }
+                    if(uniqueMonths.get(i).equals(null) && recordItemSteps.get(i).equals(null)){
+                        data.add(new ValueDataEntry("0",0));
+                    }else{
+                        Log.d("DATA", uniqueMonths.get(i) + " "+ recordItemSteps.get(i).toString());
+                        data.add(new ValueDataEntry(uniqueMonths.get(i), recordItemSteps.get(i)));
+                    }
+                }
+                 */
+
+            }
+        });
+
         bottomNavigationView.setSelectedItemId(R.id.menuStatistics);
-
-        AnyChartView statisticsChartAnyChartView = findViewById(R.id.statisticsChartAnyChartView);
-        ProgressBar progressBar = findViewById(R.id.progress_bar);
-        statisticsChartAnyChartView.setProgressBar(progressBar);
-        Cartesian cartesian = AnyChart.column();
-
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("Rouge", 80540));
-        data.add(new ValueDataEntry("Foundation", 94190));
-        data.add(new ValueDataEntry("Mascara", 102610));
-        data.add(new ValueDataEntry("Lip gloss", 110430));
-        data.add(new ValueDataEntry("Lipstick", 128000));
-        data.add(new ValueDataEntry("Nail polish", 143760));
-        data.add(new ValueDataEntry("Eyebrow pencil", 170670));
-        data.add(new ValueDataEntry("Eyeliner", 213210));
-        data.add(new ValueDataEntry("Eyeshadows", 249980));
-
-        Column column = cartesian.column(data);
-        column.tooltip()
-                .titleFormat("{%X}")
-                .position(Position.CENTER_BOTTOM)
-                .anchor(Anchor.CENTER_BOTTOM)
-                .offsetX(0d)
-                .offsetY(5d)
-                .format("${%Value}{groupsSeparator: }");
-
-        cartesian.animation(true);
-        cartesian.title("Top 10 Cosmetic Products by Revenue");
-
-        cartesian.yScale().minimum(0d);
-
-        cartesian.yAxis(0).labels().format("${%Value}{groupsSeparator: }");
-
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-        cartesian.interactivity().hoverMode(HoverMode.BY_X);
-
-        cartesian.xAxis(0).title("Product");
-        cartesian.yAxis(0).title("Revenue");
-
-        statisticsChartAnyChartView.setChart(cartesian);
-        //progressBar.setVisibility(View.GONE);
-
         bottomNavigationView.setOnNavigationItemSelectedListener((menuItem)->{
             switch(menuItem.getItemId()){
                 case R.id.menuStatistics:
@@ -89,5 +155,75 @@ public class StatisticsActivity extends AppCompatActivity {
             }
             return false;
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void onButtonWeekState(){
+        statisticsChartWeekAnyChartView.setVisibility(View.VISIBLE);
+        Cartesian cartesian = AnyChart.column();
+        List<DataEntry> data = new ArrayList<>();
+        statisticsMonthButton.setBackgroundColor(getResources().getColor(R.color.background));
+        statisticsMonthButton.setTextColor(getResources().getColor(R.color.textToday));
+
+        statisticsWeekButton.setBackgroundColor(getResources().getColor(R.color.textGoal));
+        statisticsWeekButton.setTextColor(getResources().getColor(R.color.black));
+
+        List<RecordItem> recordItemsData = RecordItem.findWithQuery(RecordItem.class, "SELECT DISTINCT date FROM RECORD_ITEM");
+        List<String> date = recordItemsData.stream().map(RecordItem::getDate).collect(Collectors.toList());
+
+        List<Integer> recordItemSteps = new ArrayList<>();
+        int steps = 0;
+
+        for (String d:date){
+            for (RecordItem recordItem:recordDao.findAll()) {
+                if(recordItem.getDate().equals(d)){
+                    steps += recordItem.getSteps();
+                }
+            }
+            recordItemSteps.add(steps);
+            steps = 0;
+        }
+
+        Collections.reverse(recordItemSteps);
+        Collections.reverse(date);
+
+        //ProgressBar progress_circular = findViewById(R.id.progress_circular);
+        //statisticsChartWeekAnyChartView.setProgressBar(progress_circular);
+
+        for (int i = 0; i < date.size(); i++){
+            if(i >= KEY_WEEK){
+                break;
+            }
+            if(date.get(i).equals(null) && recordItemSteps.get(i).equals(null)){
+                data.add(new ValueDataEntry("0",0));
+            }else{
+                data.add(new ValueDataEntry(date.get(i), recordItemSteps.get(i)));
+            }
+        }
+        Column column = cartesian.column(data);
+        column.color("purple");
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(0d)
+                .format(getResources().getString(R.string.step) +": {%Value}{groupsSeparator: }");
+
+        cartesian.animation(true);
+        cartesian.title(getResources().getString(R.string.week_activity));
+
+
+        cartesian.yScale().minimum(0d);
+
+        // cartesian.yAxis(0).labels().format("${%Value}{groupsSeparator: }");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(0).title(getResources().getString(R.string.days));
+        cartesian.yAxis(0).title(getResources().getString(R.string.steps));
+
+        statisticsChartWeekAnyChartView.setChart(cartesian);
     }
 }
